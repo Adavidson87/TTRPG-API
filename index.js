9e601280664f6cc602b95f273a95ed697bc23962
Alexander <alexander.davidson87@gmail.com>
2022-02-12 02:47:33 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// imports
+const express = require('express'),
+  morgan = rquire('morgan'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid'),
+  app = express();
+
+//
+app.use(bodyParser.json());
+
+// uses morgan to log errors in morgan's commong methods
+app.use(morgan('common'));
+
+// app uses anything in the public folder
+app.use(express.static('public'));
+
+// logs errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Error');
+});
+
+// gets main landing page
+app.get('/', (req, res) => {
+  res.send('My Characters');
+});
+
+//
+app.get('/users')
+
+// tells app to listen on port 8080
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080');
+});

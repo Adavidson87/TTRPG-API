7cb80898da6589fcfb9f9ba57f0919f251d7fa72
Alexander <alexander.davidson87@gmail.com>
2022-03-13 15:23:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,16 @@ app.get('/', (req, res) => {
 app.listen(port, '0.0.0.0', () => {
   console.log('Your app is listening on port' + port);
 });
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
